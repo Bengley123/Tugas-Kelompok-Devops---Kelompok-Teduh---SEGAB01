d2db8210b83e9a73ebc0de711d03c66057907c75
Arzaq Ajradika <arzaqajradika1@gmail.com>
2024-11-03 20:52:58 +0700

    junit test 1

---
+++ TUBES_PBO_Kelompok_9/test/test.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertNotNull;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.JComboBox;
+import javax.swing.table.DefaultTableModel;
+
+
+import view.*;
+
+/**
+ *
+ * @author arzaq
+ */
+public class test {
+    private formcrud form;
+    
+    @Before
+    public void setUp() {
+        form = new formcrud();
+    }
+    
+    @Test
+    public void testGetTextFields() {
+        // Test NIM field
+        JTextField nimField = form.gettxtNim();
+        assertNotNull("NIM field should not be null", nimField);
+        
+        // Test Nama field
+        JTextField namaField = form.gettxtNama();
+        assertNotNull("Nama field should not be null", namaField);
+        
+        // Test Kelas field
+        JTextField kelasField = form.gettxtKelas();
+        assertNotNull("Kelas field should not be null", kelasField);
+        
+        // Test Prodi field
+        JTextField prodiField = form.gettxtProdi();
+        assertNotNull("Prodi field should not be null", prodiField);
+        
+        // Test Fakultas field
+        JTextField fakultasField = form.gettxtFakultas();
+        assertNotNull("Fakultas field should not be null", fakultasField);
+        
+        // Test Angkatan field
+        JTextField angkatanField = form.gettxtAngkatan();
+        assertNotNull("Angkatan field should not be null", angkatanField);
+    }
+    
+    
+    
+    
+}
